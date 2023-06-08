dabf9ca94ce1f10d6897b95ae427789f400e0149
Kayes <kayes.ek8@gmail.com>
2023-06-08 13:32:01 +0600

    Logger Implement

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import mongoose, { ConnectOptions } from 'mongoose'
 import app from './app'
 import config from './config'
+import { errLogger, logger } from './util/logger'
 
 async function main() {
   try {
@@ -13,16 +14,16 @@ async function main() {
         } as ConnectOptions
       )
       .then(() => {
-        console.log('DB Connected!')
+        logger.info('DB Connected!')
         app.listen(config.port, () =>
-          console.log(`Server Started on Port: ${config.port}`)
+          logger.info(`Server Started on Port: ${config.port}`)
         )
       })
       .catch(error => {
-        console.error(error)
+        errLogger.error(error)
       })
   } catch (error) {
-    console.log('Failed to connect database', error)
+    errLogger.error('Failed to connect database', error)
   }
 }
 
---
+++ src/util/logger.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import path from 'path'
+import winston from 'winston'
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logger', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+})
+
+const errLogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logger', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
+
+export { logger, errLogger }
+
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+// if (process.env.NODE_ENV !== 'production') {
+//     logger.add(new winston.transports.Console({
+//         format: winston.format.simple(),
+//     }));
+// }
