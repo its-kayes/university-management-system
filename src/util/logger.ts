545d9c116329b9c1254adc290a3052051ff84076
Kayes <kayes.ek8@gmail.com>
2023-06-08 14:02:20 +0600

    Update logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,26 +1,56 @@
 /* eslint-disable no-undef */
 import path from 'path'
-import winston from 'winston'
-const logger = winston.createLogger({
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+const { combine, timestamp, label, printf } = format
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp),
+    hour = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getSeconds()
+
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UV_OK' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logger', 'winston', 'success.log'),
-      level: 'info',
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logger',
+        'winston',
+        'successes',
+        'uv_sus-%DATE%.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
 
-const errLogger = winston.createLogger({
+const errLogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UV_ERR' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logger', 'winston', 'error.log'),
-      level: 'error',
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logger',
+        'winston',
+        'errors',
+        'uv_err-%DATE%.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
@@ -31,7 +61,7 @@ export { logger, errLogger }
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
 // if (process.env.NODE_ENV !== 'production') {
-//     logger.add(new winston.transports.Console({
-//         format: winston.format.simple(),
+//     logger.add(new transports.Console({
+//         format: format.simple(),
 //     }));
 // }
