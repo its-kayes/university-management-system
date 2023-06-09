0b5718eb3cb99342028538fae30e5ea9e9c6d824
Kayes <kayes.ek8@gmail.com>
2023-06-10 01:30:32 +0600

    Validation for Semester code

--- src/modules/academic-semester/academicSemester.service.ts
+++ src/modules/academic-semester/academicSemester.service.ts
@@ -1,7 +1,14 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createSemester = async (payload: IAcademicSemester) => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new AppError('Invalid Semester Code ', httpStatus.BAD_REQUEST);
+  }
+
   const save = await AcademicSemester.create(payload);
   return save;
 };
